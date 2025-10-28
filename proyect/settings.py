81aaf5c83ab51272ef9cdb83cec5061cfa753fd4
Rebe HC <hernandezcruzrebeca767@gmail.com>
2025-10-28 08:55:50 -0600

    .

--- proyect/settings.py
+++ proyect/settings.py
@@ -25,13 +25,17 @@ SECRET_KEY = 'django-insecure-lxj96b=6%k@s_ls@)pr+$q6#6zh0%$#mdzyl8j(l%3+_x_q1m!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api-simulacion.vercel.app/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['api-simulacion.vercel.app', 'api-simulacion.onrender.com', 'localhost', '127.0.0.1']
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    'https://api-simulacion.vercel.app/',
+    'https://api-simulacion.vercel.app',
+    'https://api-simulacion.onrender.com',
 ]
 CORS_ALLOW_CREDENTIALS = True
+
+
+
 
 
 # Application definition
